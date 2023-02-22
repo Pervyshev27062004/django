bb736101a15b3e91fca435d503f9bd38af5a3b29
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-02-22 20:52:40 +0300

    Update gitignore

--- shop/products/views.py
+++ shop/products/views.py
@@ -6,4 +6,4 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     logger.info(request.GET, request.POST)
-    return HttpResponse("Shop index view")
+    return HttpResponse("Shop index viewYES")
